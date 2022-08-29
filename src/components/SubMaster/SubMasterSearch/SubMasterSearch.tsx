58ea8581af52fc36291c22e068a8bf0b6f1f5f46
chavda-bhavik <bhavikvchavda@gmail.com>
2022-08-29 09:47:59 +0530

    [feat] Reset submaster search when master data updated

--- src/components/SubMaster/SubMasterSearch/SubMasterSearch.tsx
+++ src/components/SubMaster/SubMasterSearch/SubMasterSearch.tsx
@@ -1,11 +1,17 @@
-import React, { useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Input } from "../../../components/Common"
+import { useProviderState } from "../../../context/ProviderContext"
 import { useSubMasterState } from "../../../context/SubMasterContext"
 
 const SubMasterSearch = () => {
+    const { selectedMaster } = useProviderState()
     const { getSubMastersList, t } = useSubMasterState()
     const callerRef = useRef<NodeJS.Timeout | null>(null)
     const [search, setSearch] = useState<string>("")
+
+    useEffect(() => {
+        setSearch("")
+    }, [selectedMaster])
 
     const onChangeSearch = (str: string) => {
         setSearch(str)
